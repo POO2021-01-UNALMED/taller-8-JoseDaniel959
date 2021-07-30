a31b38fbf50e9623d4e4630425bacc3b8669ab98
JoseDaniel959 <79713968+JoseDaniel959@users.noreply.github.com>
2021-07-30 15:37:08 -0500

    Add files via upload

---
+++ src/main/java/futbol/Portero.java
@@ -0,0 +1,41 @@
+package futbol;
+
+public class Portero extends Futbolista {
+	public short golesRecibidos;
+	public byte dorsal;
+	private final String posicion = "portero";
+	
+	
+	
+	public Portero() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public Portero(String nombre, int edad,short golesRecibidos, byte dorsal) {
+		super(nombre, edad, "portero");
+		this.golesRecibidos = golesRecibidos;
+		this.dorsal = dorsal;
+	}
+
+
+
+	public String toString(){
+		return "El futbolista "+this.nombre+" tiene "+this.edad+ ", y juega de "+this.posicion+" con el dorsal "+this.dorsal + "";
+		
+	}
+
+	
+	@Override
+	public int compareTo(Object o) {
+		return this.edad - ((Jugador)o).edad;
+	}
+	
+	public boolean jugarConLasManos() {
+		
+		return true;
+	}
+	
+}
